6f5cb2624590c23495c1b7dc805648dc516da18f
Ashok Cutkosky <acutkosky@yahoo-inc.com>
2017-07-26 17:20:56 -0400

    fixed some bugs

--- svrg_flow.py
+++ svrg_flow.py
@@ -6,6 +6,7 @@ from tensorflow.python.training.optimizer import Optimizer
 import tensorflow as tf
 import numpy as np
 
+
 #hacky subclass of Optimizer just so I can use make_slot...
 #not sure if it's even that helpful over a plain dict of vars...
 class SVRG(Optimizer):
@@ -27,7 +28,7 @@ class SVRG(Optimizer):
         '''
 
         if name is None:
-            name = 'SVRG_with_' + sgd_optimizer.name
+            name = 'SVRG_with_' + sgd_optimizer.get_name()
 
 
         super(SVRG, self).__init__(use_locking, name)
@@ -36,6 +37,15 @@ class SVRG(Optimizer):
         self.loss_func = loss_func
         self.computed_batch = False
         self.var_list = []
+        self._slots = {}
+
+    # def _get_or_make_slot(self, v, val, name):
+    #     if v not in self._slots:
+    #         self._slots[v] = {}
+    #     self._slots[v][name] = val
+
+    # def _get_slot(self, v, name):
+    #     return self._slots[v][name]
 
     def _create_slots(self, var_list):
         for v in var_list:
@@ -43,9 +53,9 @@ class SVRG(Optimizer):
                 batch_gradient = constant_op.constant(0,
                                               shape=v.get_shape(),
                                               dtype=v.dtype.base_dtype)
-                v_prev = tf.Variable(v)
+                v_prev = constant_op.constant(0.0,shape=v.get_shape(), dtype = v.dtype.base_dtype)
             self._get_or_make_slot(v, batch_gradient, "batch_gradient", self._name)
-            self._get_or_make_slot(v, v_prev, "v_prev", self.name)
+            self._get_or_make_slot(v, v_prev, "v_prev", self._name)
 
 
     def compute_gradients(self, var_list=None,
@@ -61,7 +71,7 @@ class SVRG(Optimizer):
 
         prev_vars = [self.get_slot(var, "v_prev") for var in var_list]
         prev_grads_and_vars = self.sgd_optimizer.compute_gradients(self.loss_func(prev_vars),
-                                                              duplicate_vars,
+                                                              prev_vars,
                                                               gate_gradients,
                                                               aggregation_method,
                                                               colocate_gradients_with_ops,
@@ -75,7 +85,6 @@ class SVRG(Optimizer):
                                                               grad_loss)
 
         def reduce_variance(grad_var, prev_grad_var):
-            self._create_slots([_get_variable_for(v) for v in var_list])
 
             (grad, var) = grad_var
             (prev_grad, prev_var) = prev_grad_var
@@ -86,18 +95,29 @@ class SVRG(Optimizer):
                 return (None, var)
             return (grad - prev_grad + batch_gradient, var)
 
-        return [reduce_variance(pair) for pair in zip(grads_and_vars, prev_grads_and_vars)]
+        return [reduce_variance(*pair) for pair in zip(grads_and_vars, prev_grads_and_vars)]
 
 
     def set_var_list(self, var_list):
         self.var_list = var_list
 
-    def recompute_batch(self, batch_loss, var_list):
+    def recompute_batch(self, batch_loss, var_list=None):
         self.computed_batch = True
+        if var_list is None:
+            var_list = self.var_list
+
+        if not hasattr(var_list, '__iter__'):
+            batch_gradients_and_vars = [var_list]
+
+        self._create_slots(var_list)
+
         batch_gradients_and_vars = tf.gradients(batch_loss, var_list)
 
+        if not hasattr(batch_gradients_and_vars, '__iter__'):
+            batch_gradients_and_vars = [batch_gradients_and_vars]
+
         batch_grad_updates = [state_ops.assign(self.get_slot(var, "batch_gradient"), grad)
-                                for grad, var in batch_gradients_and_vars]
+                                for grad, var in zip(batch_gradients_and_vars, var_list)]
         prev_vars_update = [state_ops.assign(self.get_slot(var, "v_prev"), var) for var in var_list]
 
         updates = batch_grad_updates + prev_vars_update
